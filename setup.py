f932cc14b17eeb93621f5332ad20c4b0fe80b03a
SasiVatsal <71326149+sasivatsal7122@users.noreply.github.com>
2021-12-25 09:36:33 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
  
 classifiers = [
   'Development Status :: 5 - Production/Stable',
@@ -9,16 +13,17 @@ classifiers = [
 ]
  
 setup(
-  name='ckret',
+  name='ckrett',
   version='1.0.0',
   description='a basic ciphering/deciphering tool',
-  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   url='https://github.com/sasivatsal7122/ckret-library-pypi',
   author='Sasi Vatsal',
   author_email='sasivatsal7122@gmail.com',
   license='MIT', 
   classifiers=classifiers,
-  keywords='ckret',
-  packages=find_packages(),
-  install_requires=[''] 
+  keywords='ckrett',
+  packages=['ckrett'],
+  install_requires=[''],
 )
